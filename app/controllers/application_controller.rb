09f76667193f1c6eddaec36f3365bea363785c38
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-08 10:55:22 +0200

    password protected the app in production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate :if => lambda {|c| Rails.env.production?}
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['AUTH_USERNAME'] && password == ENV['AUTH_PASSWORD']
+    end
+  end
+
 end
