9f28577237a8ae47b21ba562cd440e03ff24c1e2
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-13 21:32:57 +0200

    added validate_presence_of

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,8 @@
 class Venue < ActiveRecord::Base
   belongs_to :area
 
+  validates_presence_of :title, :address
+
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 end
