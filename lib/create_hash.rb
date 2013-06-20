12a784ee0fdaa781811fa4fcef60b55e671ed329
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-20 07:53:28 +0200

    build json from the db

--- lib/create_hash.rb
+++ lib/create_hash.rb
@@ -1,6 +1,18 @@
 require 'json'
 require File.expand_path('config/environment.rb')
 
+  def output_venue(v)
+    h = { 
+      name:      v.name,
+      address:   v.address,
+      url:       v.url,
+      latitude:  v.latitude,
+      longitude: v.longitude,
+      sun_from:  v.sun_from,
+      sun_to:    v.sun_to
+    }
+  end
+
 class CreateHash
   r = {}
   json_file = File.open('../iSola/resources/areas.json').read
@@ -12,7 +24,8 @@ class CreateHash
 
   Area.all.each do |area|
     r[:name]   = area.name
-    r[:venues] = [area.venues]
+    r[:venues] = area.venues.map { |v| output_venue(v) }
   end
   puts "Our hash is: #{r}\n\n"
+
 end
