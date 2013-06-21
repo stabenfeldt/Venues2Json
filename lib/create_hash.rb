a30c53908c873132441c22d09fe692db1ba88b40
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-21 14:42:53 +0200

    cleanup

--- lib/create_hash.rb
+++ lib/create_hash.rb
@@ -15,15 +15,20 @@ require File.expand_path('config/environment.rb')
 
 class CreateHash
   def initialize
-    @r = {}
   end
 
   def work
+    @final = []
     Area.all.each do |area|
+      puts "area: #{area.name}"
+      @r = {}
       @r[:name]   = area.name
       @r[:venues] = area.venues.map { |v| output_venue(v) }
+      puts "One is #{@r}"
+      @final << @r
     end
-    @r.to_json
+    #@r.to_json
+    @final.to_json
   end
 
 end
