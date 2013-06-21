9db89196c24fdb7a0865a874ffda4f70e1518fca
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-21 17:07:00 +0200

    avoid emptiness in the JSON file

--- lib/create_hash.rb
+++ lib/create_hash.rb
@@ -2,14 +2,16 @@ require 'json'
 require File.expand_path('config/environment.rb')
 
   def output_venue(v)
-    h = { 
+    return unless v.sun_from and v.sun_to
+    return unless v.latitude and v.longitude
+    { 
       name:      v.name,
       address:   v.address,
       url:       v.url,
       latitude:  v.latitude,
       longitude: v.longitude,
-      sun_from:  v.sun_from,
-      sun_to:    v.sun_to
+      sun_from:  (v.sun_from.strftime '%H:%M'),
+      sun_to:    (v.sun_to.strftime   '%H:%M')
     }
   end
 
@@ -22,7 +24,7 @@ class CreateHash
     Area.all.each do |area|
       @r = {}
       @r[:name]   = area.name
-      @r[:venues] = area.venues.map { |v| output_venue(v) }
+      @r[:venues] = (area.venues.map { |v| output_venue(v) }).compact
       @final << @r
     end
     @final.to_json
