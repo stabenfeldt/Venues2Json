82ffbac2198b76a97559053ce3be122994de1d9d
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-06-21 14:41:25 +0200

    s/title/name

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,8 +1,8 @@
 class Venue < ActiveRecord::Base
   belongs_to :area
 
-  validates_presence_of    :title, :address
-  validates_uniqueness_of  :title, :address
+  validates_presence_of    :name, :address
+  validates_uniqueness_of  :name, :address
 
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
